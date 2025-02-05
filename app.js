c64347e6acabea7f72a447d9d1a09cba0b8534a0
onboni <onni.luova@gmail.com>
2025-02-05 07:50:45 +0200

    Added cors middleware to app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 
 const v1Router = express.Router();
 
